22101d06b8d4851d370b39e059126e62d00e9375
Mohd Ibrahim Ahmed <mohdabrar41111@gmail.com>
2025-05-10 03:24:09 +0530

    Update BreadCrumb.tsx

--- src/app/components/BreadCrumb.tsx
+++ src/app/components/BreadCrumb.tsx
@@ -11,10 +11,10 @@ const Breadcrumb: React.FC<BreadcrumbProps> = ({ title, subtitle }) => {
   return (
     <div className="relative bg-primary min-h-[190px] flex items-end pb-0 mb-24">
       <div className="w-full absolute bottom-[-60px] md:bottom-[-100px] px-4 sm:px-20">
-        <div className="max-w-3xl mx-auto bg-white dark:bg-gray-900 rounded-2xl shadow-xl px-12 py-12 md:py-14 text-center">
-          <h1 className="text-4xl md:text-5xl font-semibold text-gray-800 dark:text-white">{title}</h1>
+        <div className="max-w-3xl mx-auto bg-white rounded-2xl shadow-xl px-12 py-12 md:py-14 text-center">
+          <h1 className="text-4xl md:text-5xl font-semibold text-gray-800">{title}</h1>
           {subtitle && (
-            <p className="mt-4 text-gray-500 dark:text-gray-300 font-serif text-sm sm:text-base">
+            <p className="mt-4 text-gray-500 font-serif text-sm sm:text-base">
               {subtitle}
             </p>
           )}
